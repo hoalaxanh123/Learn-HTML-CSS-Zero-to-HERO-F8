527a6db417ccf55e3d042988a80131b4c6140eb9
Vuong Nguyen <nguyenvanvuong972@gmail.com>
2022-03-19 10:19:36 +0700

    W3: fix

--- w3_the_band/assets/javascript/javascript.js
+++ w3_the_band/assets/javascript/javascript.js
@@ -1,49 +1,43 @@
-let isOpeningMenu = false
+let isOpeningMenu = false;
 
+const navItems = document.querySelectorAll(".navigator li a");
 
-const navItems = document.querySelectorAll(".navigator li a")
-
-
-navItems.forEach(element => {
-    let isSubnavSwitcher = element.nextElementSibling && element.nextElementSibling.classList.contains("sub-nav")
+navItems.forEach((element) => {
+    let isSubnavSwitcher =
+        element.nextElementSibling &&
+        element.nextElementSibling.classList.contains("sub-nav");
 
     element.addEventListener("click", (event) => {
         if (isSubnavSwitcher) {
-            event.preventDefault()
+            event.preventDefault();
         } else {
             if (header.classList.contains("height-auto")) {
-                header.classList.remove("height-auto")
+                header.classList.remove("height-auto");
             }
         }
-
-    })
+    });
 });
 
-
-
-const switcherMobileMenu = document.querySelector(".mobile-menu-switcher")
-const header = document.querySelector(".header")
+const switcherMobileMenu = document.querySelector(".mobile-menu-switcher");
+const header = document.querySelector(".header");
 
 switcherMobileMenu.addEventListener("click", () => {
-
     if (header.classList.contains("height-auto")) {
-        header.classList.remove("height-auto")
+        header.classList.remove("height-auto");
     } else {
-        header.classList.add("height-auto")
+        header.classList.add("height-auto");
     }
     isOpeningMenu = !isOpeningMenu;
+});
 
-})
-
-
-let ticketLeft = localStorage.getItem("ticket-left")
+let ticketLeft = localStorage.getItem("ticket-left");
 if (!ticketLeft) {
-    localStorage.setItem("ticket-left", 10)
-    ticketLeft = 10
+    localStorage.setItem("ticket-left", 10);
+    ticketLeft = 10;
 }
 
-const ticketLeftElement = document.getElementById("ticket-left")
-ticketLeftElement.innerHTML = ticketLeft
+const ticketLeftElement = document.getElementById("ticket-left");
+ticketLeftElement.innerHTML = ticketLeft;
 
 const closeModal = () => {
     modalBuyTicket.classList.remove("modal-open");
@@ -55,27 +49,25 @@ const openModal = () => {
 };
 const buyTicket = () => {
     if (ticketLeft > 0) {
-        alert('Thank you so much !')
-        ticketLeft = ticketLeft - 1
-        localStorage.setItem("ticket-left", ticketLeft)
+        alert("Thank you so much !");
+        ticketLeft = ticketLeft - 1;
+        localStorage.setItem("ticket-left", ticketLeft);
     } else {
-        alert('Oops! We run out of ticket!')
+        alert("Oops! We run out of ticket!");
     }
-    const ticketLeftElement = document.getElementById("ticket-left")
-    ticketLeftElement.innerHTML = ticketLeft
+    const ticketLeftElement = document.getElementById("ticket-left");
+    ticketLeftElement.innerHTML = ticketLeft;
 
-    closeModal()
-
-}
+    closeModal();
+};
 
 const modalBuyTicket = document.querySelector(".modal");
 const modalContainerBuyTicket = document.querySelector(".modal-container");
 
-modalBuyTicket.addEventListener("click", closeModal)
+modalBuyTicket.addEventListener("click", closeModal);
 modalContainerBuyTicket.addEventListener("click", (e) => {
-    e.stopPropagation()
-})
-
+    e.stopPropagation();
+});
 
 const buyTicketBtns = document.querySelectorAll(".buy-ticket");
 for (const buyTicketBtn of buyTicketBtns) {
